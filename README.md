8e55e9977383105a26b3dba60fa36747e7e25f38
jin <jin@motemote.kr>
2025-06-09 12:04:34 +0900

    chore : 캐러셀 업데이트

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # js_template
 
-
-
 ## carousel
 
 <p align="center">
@@ -9,18 +7,25 @@
 </p>
 
 ```html
+
 <html>
-  <body>
-    <script>
-      const ratio = 16 / 9;
-      const images = [
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" rel="stylesheet"/>
+</head>
+<body>
+<script>
+    const ratio = 16 / 9;
+    const images = [
         "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",
         "https://placehold.co/600x300/orange/white",
         "https://placehold.co/600x300/orange/white",
-      ];
-    </script>
-    <div class="carousel-container"></div>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/allcle-projects/js_template@0.0.9/carousel/carousel.js"></script>
-  </body>
+    ];
+</script>
+<div class="carousel-container"></div>
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/gh/allcle-projects/js_template@0.1.0/carousel/carousel.js"></script>
+</body>
 </html>
 ```
--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,227 +1,122 @@
 if (images.length !== 0) {
-  const wrapper = document.createElement("div");
-  wrapper.innerHTML = `
-  <!DOCTYPE html>
-  <html lang="ko">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <style>
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
+    const container = document.querySelector(".carousel-container");
 
-        html {
-          -webkit-text-size-adjust: 100%;
-          -webkit-tap-highlight-color: transparent;
-          -webkit-touch-callout: none;
-        }
-  
-        ul {
-          list-style: none;
-        }
-  
-        .store-carousel-container {
-          position: relative;
-          touch-action: pan-y;
-        }
+    const wrapper = document.createElement("div");
+    wrapper.className = "swiper";
+    wrapper.style.width = "100%";
+    wrapper.style.position = "relative";
+    wrapper.style.paddingTop = `${(1 / ratio) * 100}%`; // 비율 유지용 padding-top
 
-        .store-carousel-carousel {
-          width: 100%;
-          height: 0px;
-          display: flex;
-          justify-content: space-between;
-          overflow: hidden;
-          position: relative;
-          background: #ffffff;
-        }
-        .store-carousel-slides {
-          display: flex;
-          width: 100%;
-          height: 100%;
-        }
-        .store-carousel-slide {
-          flex: 1 0 auto;
-          width: 100%;
-          height: 100%;
-          position: relative;
-          margin: 0;
-        }
-        .store-carousel-slide img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-          pointer-events: none;
-        }
-        .store-carousel-action-box {
-          position: absolute;
-          top: 50%;
-          transform: translateY(-50%);
-          display: flex;
-          justify-content: space-between;
-          width: 100%;
-          flex-basis: auto;
-          padding: 0 16px;
-          pointer-events: none;
-        }
-        .store-carousel-prev-button,
-        .store-carousel-next-button {
-          display: flex;
-          min-width: auto;
-          align-items: center;
-          justify-content: center;
-          height: 36px;
-          width: 36px;
-          border-radius: 28px;
-          border: 1px solid #e8e8f4;
-          background: #ffffff;
-          opacity: 0.5;
-          box-shadow: 0px 4px 12px 0px rgba(110, 109, 120, 0.16);
-          pointer-events: all;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="store-carousel-container">
-        <div class="store-carousel-carousel">
-          <ul class="store-carousel-slides"></ul>
-        </div>
-        <div class="store-carousel-action-box">
-          <button class="store-carousel-prev-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M12.0284 15.4717L12.9712 14.5289L8.4426 10.0003L12.9712 5.47168L12.0284 4.52887L6.55698 10.0003L12.0284 15.4717Z"
-                fill="#ACAABB"
-              />
-            </svg>
-          </button>
-          <button class="store-carousel-next-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 20 20"
-              fill="none"
-            >
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M7.97162 15.4717L7.02881 14.5289L11.5574 10.0003L7.02881 5.47168L7.97162 4.52887L13.443 10.0003L7.97162 15.4717Z"
-                fill="#ACAABB"
-              />
-            </svg>
-          </button>
-        </div>
-      </div>
-    </body>
-  </html>
-   `;
+    wrapper.innerHTML = `
+    <style>
+      * {
+        margin: 0; padding: 0; box-sizing: border-box;
+      }
+      .swiper {
+        width: 100%;
+        aspect-ratio: ${ratio};
+        position: relative;
+        background: #fff;
+      }
+      .swiper-wrapper {
+        position: absolute;
+        top: 0; left: 0;
+        width: 100%; height: 100%;
+      }
+      .swiper-slide {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+        width: 100%; height: 100%;
+      }
+      .swiper-slide img,
+      .swiper-slide video {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        pointer-events: none;
+      }
+      .swiper-button-prev,
+      .swiper-button-next {
+        position: absolute;
+        color: #000;
+        width: 32px;
+        height: 32px;
+        top: calc(50% + 16px);
+        transform: translateY(-50%);
+        background: rgba(255, 255, 255, 0.8);
+        border-radius: 50%;
+        transition: opacity 1s ease;
+      }
+      .swiper-button-disabled {
+          display: none;
+      }
+      .swiper-button-prev:hover,
+      .swiper-button-next:hover {
+          opacity: 0.8;
+      }
+      .swiper-button-prev::after,
+      .swiper-button-next::after {
+          font-size: 14px;
+      }
+      
+      .swiper-pagination {
+        position: absolute;
+        bottom: 8px !important;
+        left: 8px !important;
+        pointer-events: auto !important;
+        gap: 4px;
+        width: 100%;
+        z-index: 20;
+        display: flex;
+        flex-direction: row;
+      }
+       
+      .swiper-pagination-bullet {
+         margin: 0 !important;
+         width: 24px;
+         height: 4px;
+         border-radius: 4px;
+      }
+    
+      .swiper-pagination-bullet-active {
+        background: #000
+      }
+    </style>
 
-  document.querySelector(".carousel-container").append(wrapper);
-  const carousel = document.querySelector(".store-carousel-carousel");
-  const slides = document.querySelector(".store-carousel-slides");
-  const prevButton = document.querySelector(".store-carousel-prev-button");
-  const nextButton = document.querySelector(".store-carousel-next-button");
+    <div class="swiper-wrapper">
+      ${images
+        .map((src) => {
+            const isVideo = /\.(mp4|webm|ogg|mov)$/i.test(src);
+            return `
+            <div class="swiper-slide">
+              ${
+                isVideo
+                    ? `<video src="${src}" autoplay muted loop playsinline></video>`
+                    : `<img src="${src}" alt="carousel image" />`
+            }
+            </div>`;
+        })
+        .join("")}
+    </div>
+    <div class="swiper-pagination"></div>
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+  `;
 
-  carousel.style.height = 'auto';
-  carousel.style.aspectRatio = `${ratio}`;
-  images.forEach((mediaSrc) => {
-    const slide = document.createElement("li");
-    slide.classList.add("store-carousel-slide");
+    container.appendChild(wrapper);
 
-    const isVideo = /\.(mp4|webm|ogg|mov)$/i.test(mediaSrc);
-
-    if (isVideo) {
-      const video = document.createElement("video");
-      video.src = mediaSrc;
-      video.muted = true;
-      video.playsInline = true;
-      video.autoplay = true;
-      video.loop = true;
-      video.style.width = "100%";
-
-      slide.appendChild(video);
-    } else {
-      const image = document.createElement("img");
-      image.src = mediaSrc;
-      image.style.width = "100%";
-
-      slide.appendChild(image);
-    }
-
-    slides.appendChild(slide);
-  });
-
-  const clamp = (num, a, b) =>
-    Math.max(Math.min(num, Math.max(a, b)), Math.min(a, b));
-  const adjustSlide = () => {
-    isMouseDown = false;
-    const percent = (getTranslateXPercent(slides) / window.innerWidth) * 100;
-    const slideIndex = Math.abs(Math.round(percent / 100));
-    showSlide(slideIndex);
-  };
-  const getTranslateXPercent = (element) => {
-    const style = window.getComputedStyle(element);
-    const matrix = new WebKitCSSMatrix(style.transform);
-    return matrix.m41;
-  };
-
-  let isMouseDown = false;
-  let currentSlide = 0;
-  let startX = 0;
-  const showSlide = (n) => {
-    if (n === 0) {
-      prevButton.style.visibility = "hidden";
-    } else {
-      prevButton.style.visibility = "visible";
-    }
-    if (n === slides.children.length - 1) {
-      nextButton.style.visibility = "hidden";
-    } else {
-      nextButton.style.visibility = "visible";
-    }
-    slides.style.transition = `transform 0.7s ease-in-out`;
-    slides.style.transform = `translateX(${-100 * n}%)`;
-    currentSlide = n;
-  };
-  const moveOffset = (event) => {
-    if (isMouseDown) {
-      const deltaX = event.clientX - startX;
-      const left = carousel.scrollLeft - deltaX;
-      const translateX = (left / carousel.clientWidth) * 100;
-      const currentX = (getTranslateXPercent(slides) / window.innerWidth) * 100;
-      const toOffset = currentX - translateX;
-      const maxOffset = 0;
-      const minOffset = -(100 * (slides.children.length - 1));
-      const offset = clamp(toOffset, minOffset, maxOffset);
-      slides.style.transition = `transform 0.0s ease-in-out`;
-      slides.style.transform = `translateX(${offset}%)`;
-      startX = event.clientX;
-    }
-  };
-  const setStartOffset = (event) => {
-    isMouseDown = true;
-    startX = event.clientX;
-  };
-  prevButton.addEventListener("click", () => {
-    showSlide(Math.max(currentSlide - 1, 0));
-  });
-  nextButton.addEventListener("click", () => {
-    showSlide(Math.min(currentSlide + 1, slides.children.length - 1));
-  });
-  carousel.addEventListener("pointerdown", setStartOffset);
-  carousel.addEventListener("pointermove", moveOffset);
-  document.addEventListener("pointerup", adjustSlide);
-  document.addEventListener("pointerleave", adjustSlide);
-  showSlide(0);
+    new Swiper(".swiper", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        slidesPerView: 1,
+        loop: false,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
 }
